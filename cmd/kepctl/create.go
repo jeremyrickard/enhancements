aea2f98426a2c6f5d334b14eb8f26a3a7d09ffb2
Jeremy Rickard <rickardj@vmware.com>
2020-11-15 17:54:37 -0700

    Rough POC for keptctl enhancements
    Signed-off-by: Jeremy Rickard <rickardj@vmware.com>

--- cmd/kepctl/create.go
+++ cmd/kepctl/create.go
@@ -17,18 +17,32 @@ limitations under the License.
 package main
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"k8s.io/enhancements/pkg/kepctl"
 )
 
 func buildCreateCommand(k *kepctl.Client) *cobra.Command {
 
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create a resource [kep, reciept]",
+		Long:  "Create a new enhancements resource, either a kep or a reciept for a given release.",
+	}
+	cmd.AddCommand(buildCreateKEPCommand(k))
+	cmd.AddCommand(buildCreateReceiptCommand(k))
+	return cmd
+}
+
+func buildCreateKEPCommand(k *kepctl.Client) *cobra.Command {
 	opts := kepctl.CreateOpts{}
 	cmd := &cobra.Command{
-		Use:     "create [KEP]",
+		Use:     "kep [KEP]",
 		Short:   "Create a new KEP",
 		Long:    "Create a new KEP using the current KEP template for the given type",
-		Example: `  kepctl create sig-architecture/000-mykep`,
+		Example: `  kepctl create kep sig-architecture/000-mykep`,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			return opts.Validate(args)
 		},
@@ -47,5 +61,33 @@ func buildCreateCommand(k *kepctl.Client) *cobra.Command {
 	f.StringArrayVar(&opts.PRRApprovers, "prr-approver", []string{}, "PRR Approver")
 
 	addRepoPathFlag(f, &opts.CommonArgs)
+	return cmd
+}
+
+func buildCreateReceiptCommand(k *kepctl.Client) *cobra.Command {
+	opts := struct {
+		Release string
+		Stage   string
+	}{}
+	cmd := &cobra.Command{
+		Use:     "receipt [KEP]",
+		Short:   "Target a KEP for a release",
+		Long:    "Target a KEP for a release",
+		Example: `  kepctl create receipt sig-architecture/000-mykep --release v1.22 --stage beta`,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return errors.New("KEP is required - ex: sig-architecture/000-mykep")
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Fprintf(k.Out, "this would create a new receipt in the proposed folder for release %s\n", opts.Release)
+			return nil
+		},
+	}
+
+	f := cmd.Flags()
+	f.StringVar(&opts.Release, "release", "", "Release To Target")
+	f.StringVar(&opts.Stage, "stage", "", "Stage KEP will be promoted to")
 	return cmd
 }
